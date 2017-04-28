f4562e82dfff209574a6d4dfb9d6e49eccefaf36
ARIEL REY <ariel.rey@mercadolibre.com>
2017-04-28 16:28:57 -0300

    Updating webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,13 @@ const path = require('path');
 
 module.exports = {
   // the entry file for the bundle
-  entry: path.join(__dirname, '/app/client/index.js'),
+  entry: {
+    index: path.join(__dirname, '/app/client/index.js')
+  },
   // the bundle file we will get in the result
   output: {
     path: path.join(__dirname, 'bundles'),
-    filename: 'app.js',
+    filename: '[name].js',
   },
   module: {
     // apply loaders to files that meet given conditions
