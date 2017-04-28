171c6112703e13f4575bafa7c4542850fe6def7d
ARIEL REY <ariel.rey@mercadolibre.com>
2017-04-28 16:34:46 -0300

    Refactoring webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,23 @@
  * Created by arey on 4/28/17.
  */
 const path = require('path');
+const baseDirectory = __dirname;
 
 module.exports = {
   // the entry file for the bundle
   entry: {
-    index: path.join(__dirname, '/app/client/index.js')
+    index: path.join(baseDirectory, '/app/client/index.js')
   },
   // the bundle file we will get in the result
   output: {
-    path: path.join(__dirname, 'bundles'),
+    path: path.join(baseDirectory, 'bundles'),
     filename: '[name].js',
   },
   module: {
     // apply loaders to files that meet given conditions
     loaders: [{
       test: /\.js?$/,
-      include: path.join(__dirname, '/app'),
+      include: path.join(baseDirectory, '/app'),
       loader: 'babel-loader',
       query: {
         presets: ["react", "es2015"],
