2921601278fa1925e4ed3d167e3c8fdb244935df
ARIEL REY <ariel.rey@mercadolibre.com>
2017-08-30 12:37:11 -0300

    Updating index

--- app/client/index.js
+++ app/client/index.js
@@ -5,6 +5,6 @@ const { render } = require('./utils/renderer');
 
 // You need to load the LESS here because this is the entry point on Webpack
 require('../pages/index/index.less');
-const IndexView = require('../pages/index/index');
+const IndexView = require('../pages/index');
 
 render(IndexView);
