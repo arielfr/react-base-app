4a52017b9e6043596d1206f5c2bdbf173d5935da
ARIEL REY <ariel.rey@mercadolibre.com>
2017-11-28 17:27:01 -0300

    Adding tests dependencies and basic files

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -2,19 +2,20 @@
  * Created by arey on 2/14/17.
  */
 const React = require('react');
+const PropTypes = require('prop-types');
 const Head = require('react-declarative-head');
 
-module.exports = (props) => {
-  const pageTitle = props.pageTitle || 'React Base App';
-  const pageDescription = props.pageDescription || 'React Base Application With Hot Module Replacing';
+const Layout = (props) => {
+  const pageTitle = props.pageTitle || 'Twitter Searcher';
+  const pageDescription = props.pageDescription || 'Twitter Searcher using React Server Side Rendering';
 
   return (
     <div className="App">
       <Head>
         <title>{pageTitle}</title>
         <link rel="shortcut icon" href="favicon.ico" />
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content={pageDescription} />
       </Head>
       <div id="root">
@@ -23,3 +24,23 @@ module.exports = (props) => {
     </div>
   );
 };
+
+
+/**
+ * Prop Types
+ */
+Layout.propTypes = {
+  pageTitle: PropTypes.string,
+  pageDescription: PropTypes.string,
+};
+
+/**
+ * Default Props
+ */
+
+Layout.defaultProps = {
+  pageTitle: '',
+  pageDescription: '',
+};
+
+module.exports = Layout;
